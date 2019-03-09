2783e3819c643f6a1c4adbfeabc403fae79c2fd0
Jay Desai <jaydesai21289@gmail.com>
2019-03-09 17:05:51 -0500

    - Finally,  All good with sidebar at this point . - app.module does have error with platform-browser not getting import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule} from '@angular/platform-browser/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    SidebarComponent
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    CommonModule
   ],
-  exports: [ ],
+  exports: [CommonModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
